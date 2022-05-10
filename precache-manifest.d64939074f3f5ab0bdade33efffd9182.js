cfdfbf3b18164f4d5f45994b2d4b38b1fce6e223
saqib <saqibraheem1999@gmail.com>
2022-05-10 16:15:32 +0500

    Updates

---
+++ precache-manifest.d64939074f3f5ab0bdade33efffd9182.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc73415e3cd9a881b768",
+    "url": "/ToDo-React/static/css/main.9529128a.chunk.css"
+  },
+  {
+    "revision": "fc73415e3cd9a881b768",
+    "url": "/ToDo-React/static/js/main.fc73415e.chunk.js"
+  },
+  {
+    "revision": "3d7694739b4ac8b7bb36",
+    "url": "/ToDo-React/static/js/1.3d769473.chunk.js"
+  },
+  {
+    "revision": "1d92d76474bf8f35d3cd",
+    "url": "/ToDo-React/static/js/2.1d92d764.chunk.js"
+  },
+  {
+    "revision": "0f95f414ec3420279423",
+    "url": "/ToDo-React/static/js/runtime~main.0f95f414.js"
+  },
+  {
+    "revision": "119183b4c1114cae0a155da60c1a3a63",
+    "url": "/ToDo-React/index.html"
+  }
+];
